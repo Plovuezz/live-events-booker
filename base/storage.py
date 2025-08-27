79d6bb82f9eecf438ba65beb6653c3c435ed5438
Vovas <vovaafon25@gmail.com>
2025-08-28 02:02:06 +0300

    trying to fix media2

--- base/storage.py
+++ base/storage.py
@@ -5,20 +5,20 @@ from storages.backends.dropbox import DropboxStorage
 from storages.utils import safe_join
 
 
-class WindowsCompatibleDropboxStorage(DropboxStorage):
-    CACHE_TTL = 60 * 60 * 2
-
-    @staticmethod
-    def _get_cache_key(name: str) -> str:
-        return f"dropbox:media:{name}"
-
-    def url(self, name):
-        cache_key = self._get_cache_key(name)
-
-        if link := cache.get(cache_key):
-            return link
-
-        link = super().url(name)
-        cache.set(cache_key, link, self.CACHE_TTL)
-
-        return link
+# class WindowsCompatibleDropboxStorage(DropboxStorage):
+#     CACHE_TTL = 60 * 60 * 2
+#
+#     @staticmethod
+#     def _get_cache_key(name: str) -> str:
+#         return f"dropbox:media:{name}"
+#
+#     def url(self, name):
+#         cache_key = self._get_cache_key(name)
+#
+#         if link := cache.get(cache_key):
+#             return link
+#
+#         link = super().url(name)
+#         cache.set(cache_key, link, self.CACHE_TTL)
+#
+#         return link
