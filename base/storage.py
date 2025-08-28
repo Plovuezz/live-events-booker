32ae92c6d98ccbcb6fccfb991e4a0c35dcb50882
Volodymyr <vovaafon25@gmail.com>
2025-08-28 18:11:24 +0300

    Update storage.py

--- base/storage.py
+++ base/storage.py
@@ -5,20 +5,20 @@ from storages.backends.dropbox import DropboxStorage
 from storages.utils import safe_join
 
 
- class WindowsCompatibleDropboxStorage(DropboxStorage):
-     CACHE_TTL = 60 * 60 * 2
+class WindowsCompatibleDropboxStorage(DropboxStorage):
+    CACHE_TTL = 60 * 60 * 2
 
-     @staticmethod
-     def _get_cache_key(name: str) -> str:
-         return f"dropbox:media:{name}"
+    @staticmethod
+    def _get_cache_key(name: str) -> str:
+        return f"dropbox:media:{name}"
 
-     def url(self, name):
-         cache_key = self._get_cache_key(name)
+    def url(self, name):
+        cache_key = self._get_cache_key(name)
 
-         if link := cache.get(cache_key):
-             return link
+        if link := cache.get(cache_key):
+            return link
 
-         link = super().url(name)
-         cache.set(cache_key, link, self.CACHE_TTL)
+        link = super().url(name)
+        cache.set(cache_key, link, self.CACHE_TTL)
 
-         return link
+        return link
