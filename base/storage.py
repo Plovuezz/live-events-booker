5cc57c8d834f0d0a2aace6f52a42b0a64255f5d9
Vovas <vovaafon25@gmail.com>
2025-08-31 18:42:12 +0300

    added password recovery pages

--- base/storage.py
+++ base/storage.py
@@ -1,6 +1,9 @@
+import os
 
 from django.core.cache import cache
+from django.utils._os import safe_join
 from storages.backends.dropbox import DropboxStorage
+
 
 
 class WindowsCompatibleDropboxStorage(DropboxStorage):
@@ -20,3 +23,13 @@ class WindowsCompatibleDropboxStorage(DropboxStorage):
         cache.set(cache_key, link, self.CACHE_TTL)
 
         return link
+
+    # def _full_path(self, name):
+    #     if name == "/":
+    #         name = ""
+    #
+    #     # If the machine is windows do not append the drive letter to file path
+    #     if os.name == "nt":
+    #         return os.path.join("/", self.root_path, name).replace("\\", "/")
+    #     else:
+    #         return safe_join(self.root_path, name).replace("\\", "/")
